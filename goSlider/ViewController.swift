d61eadc72a08b38b7a9c3908900fc558d5ec3c0a
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2017-07-14 11:43:03 +0200

    Initial Commit

---
+++ goSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goSlider
+//
+//  Created by Thomas Guilleminot on 14/07/2017.
+//  Copyright © 2017 Thomas Guilleminot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
